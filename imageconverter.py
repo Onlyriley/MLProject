115dfd6c351d21acd4e2b667ca58ccc506ddb9be
Onlyriley <90656278+Onlyriley@users.noreply.github.com>
2024-04-10 10:40:57 -0400

    Finished Image Converter
    Whoops, I meant ChatGPT finished the image converter. Images from the inputs folder are converted to 32x32x3 array and pickled into output.pkl

--- imageconverter.py
+++ imageconverter.py
@@ -1,7 +1,6 @@
-from PIL import Image
-import numpy as np
-from numpy import asarray
 import os
+import cv2
+import numpy as np
 import pickle
 
 '''
@@ -10,13 +9,25 @@ machine learning models can make predictions with them
 
 This script doesnt work quite yet but the initiative is there
 '''
+# Function to resize image to 32x32
+def resize_image(image):
+    return cv2.resize(image, (32, 32))
 
-all_images = np.array()
+# Function to load images from folder, resize, and convert to numpy array
+def load_images(folder_path):
+    images = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg"):
+            image_path = os.path.join(folder_path, filename)
+            image = cv2.imread(image_path)
+            if image is not None:
+                resized_image = resize_image(image)
+                images.append(resized_image)
+    return np.array(images)
 
-files = os.listdir("./input")
-for file in files:
-    image = Image.open(file)
-    resized_image = image.resize((32,32))
-    numpydata = asarray(resized_image)
-    all_images.append(numpydata)
-    
+input_folder = "./input"
+output_file = "output.pkl"
+images = load_images(input_folder)
+
+with open(output_file, 'wb') as f:
+    pickle.dump(images, f)
