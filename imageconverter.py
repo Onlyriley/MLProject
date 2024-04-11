747844076bfeaecb4455b88944231bb476d714f8
Onlyriley <rileysimmons996@gmail.com>
2024-04-10 23:44:44 -0400

    New training set
    Added cars dataset

--- imageconverter.py
+++ imageconverter.py
@@ -27,8 +27,15 @@ def load_images(folder_path):
     return np.array(images)
 
 input_folder = "./input"
+train_folder = "./traindata"
 output_file = "output.pkl"
+train_output_file = "train.pkl"
 images = load_images(input_folder)
 
 with open(output_file, 'wb') as f:
     pickle.dump(images, f)
+
+train_images = load_images(train_folder)
+
+with open(train_output_file, 'wb') as f:
+    pickle.dump(train_images, f)
