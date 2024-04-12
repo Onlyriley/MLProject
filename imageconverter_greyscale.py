bb0d6701d318551517378c1dba5646bed3cf1504
Matthew Yurkunas <myurkunas@gmail.com>
2024-04-12 10:07:45 -0400

    Working on greyscale images and GridSearchCV

---
+++ imageconverter_greyscale.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import numpy as np
+import pickle
+
+'''
+This file is for taking images and converting them so that the
+machine learning models can make predictions with them.
+
+This also turns images greyscale for less complex training on models.
+
+This script doesnt work quite yet but the initiative is there
+'''
+# Function to resize image to 32x32
+def resize_image(image):
+    blurred_image = cv2.blur(image, (10, 10))
+    gray_blurred_image = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2GRAY) 
+    return cv2.resize(gray_blurred_image, (32, 32))
+
+# Function to load images from folder, resize, and convert to numpy array
+def load_images(folder_path):
+    images = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg"):
+            image_path = os.path.join(folder_path, filename)
+            image = cv2.imread(image_path)
+            if image is not None:
+                resized_image = resize_image(image)
+                images.append(resized_image)
+    return np.array(images)
+
+input_folder = "./input"
+train_folder = "./traindata"
+output_file = "output.pkl"
+train_output_file = "train.pkl"
+images = load_images(input_folder)
+
+with open(output_file, 'wb') as f:
+    pickle.dump(images, f)
+
+#train_images = load_images(train_folder)
+
+#with open(train_output_file, 'wb') as f:
+ #   pickle.dump(train_images, f)
